d006c58450c4a3b700cde65920af1ffcd4bc47d7
niku1908 <nikhilaggarwal298@gmail.com>
2026-06-22 07:47:11 +0000

    commit

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'mvn clean install -DskipTests'
+    sh './mvnw clean install -DskipTests'
 }
--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'mvn verify -DskipUnitTests'
+    sh './mvnw verify -DskipUnitTests'
 }
